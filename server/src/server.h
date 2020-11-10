5edc1b0017b8ae12923881a97512a5fac1b48cd8
Elret <dev@noun37.net>
2020-11-10 04:28:45 -0800

    Init server build

---
+++ server/src/server.h
@@ -0,0 +1,13 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <iostream>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string>
+#include <pthread.h>
+#include <stdlib.h>
+#include <cstring>
+
+#endif
