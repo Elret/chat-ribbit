b5eedd1c04c6ca4c812c29813f1cc9da98aa72b7
Elret <dev@noun37.net>
2020-11-11 03:14:20 -0800

    End of Sockets. Starting with boost::asio

--- server/src/server.h
+++ server/src/server.h
@@ -2,12 +2,7 @@
 #define SERVER_H
 
 #include <iostream>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h>
+#include <boost/asio.hpp>
 #include <string>
-#include <pthread.h>
-#include <stdlib.h>
-#include <cstring>
 
 #endif
